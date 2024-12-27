367d69793ff972ac80bfc0bb1977f58d387dc6cf
vedataltun <rttvedudi@gmail.com>
2024-12-27 05:46:09 +0300

    update done

--- src/screens/LikedSongScreen.jsx
+++ src/screens/LikedSongScreen.jsx
@@ -64,138 +64,136 @@ const LikedSongScreen = () => {
     <>
       <LinearGradient colors={['#614385', '#516395']} style={{flex: 1}}>
         <SafeAreaView>
-          <ScrollView>
-            <View
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <TouchableOpacity style={{marginHorizontal: 10}}>
+              <Ionicons name="arrow-back" size={25} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity
               style={{
                 flexDirection: 'row',
                 justifyContent: 'space-between',
                 alignItems: 'center',
-              }}>
-              <TouchableOpacity style={{marginHorizontal: 10}}>
-                <Ionicons name="arrow-back" size={25} color="white" />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  marginHorizontal: 10,
-                  marginTop: 9,
-                }}>
-                <TouchableOpacity
-                  style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    gap: 10,
-                    padding: 9,
-                    // flex: 1,
-                    height: 38,
-                    backgroundColor: '#42275a',
-                    borderRadius: 8,
-                  }}>
-                  <TextInput
-                    placeholderTextColor={'white'}
-                    style={{
-                      fontWeight: '500',
-                      width: '85%',
-                      color: 'white',
-                    }}
-                    onChangeText={setSearchText}
-                    onSubmitEditing={{handleSearch}}
-                    placeholder="Find in Liked songs"
-                  />
-                  <AntDesign name="search1" size={25} color="white" />
-                </TouchableOpacity>
-              </TouchableOpacity>
-            </View>
-            <View style={{height: 50}} />
-            <View style={{marginHorizontal: 10}}>
-              <Text style={{fontSize: 18, color: 'white', fontWeight: 'bold'}}>
-                {' '}
-                Liked Songs
-              </Text>
-              <Text style={{fontSize: 13, color: 'white', marginTop: 5}}>
-                {' '}
-                {searchedTracks.length} Songs
-              </Text>
-            </View>
-            <Pressable
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
                 marginHorizontal: 10,
+                marginTop: 9,
               }}>
               <TouchableOpacity
-                style={{
-                  height: 30,
-                  width: 30,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  backgroundColor: '#1DB954',
-                  borderRadius: 18,
-                }}>
-                <AntDesign name="arrowdown" size={20} color="white" />
-              </TouchableOpacity>
-              <View
                 style={{
                   flexDirection: 'row',
                   alignItems: 'center',
                   gap: 10,
+                  padding: 9,
+                  // flex: 1,
+                  height: 38,
+                  backgroundColor: '#42275a',
+                  borderRadius: 8,
                 }}>
-                <TouchableOpacity>
-                  <FontAwesome6
-                    name="arrow-down-up-across-line"
-                    size={25}
-                    color="#1DB954"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
+                <TextInput
+                  placeholderTextColor={'white'}
                   style={{
-                    height: 60,
-                    width: 60,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    backgroundColor: '#1DB954',
-                    borderRadius: 30,
-                  }}>
-                  <Entypo name="controller-play" size={30} color="white" />
-                </TouchableOpacity>
-              </View>
-            </Pressable>
-            {loading ? (
-              <ActivityIndicator size={'large'} color={'gray'} />
-            ) : (
-              <FlatList
-                data={searchedTracks}
-                // keyExtractor={item => item.track.key}
-                style={{marginTop: 10}}
-                renderItem={({item}) => (
-                  <Pressable
-                  // onPress={() => handlePlay(item)}
-                  >
-                    <View style={styles.trackContainer}>
-                      {/* <Image
-                        source={{uri: item.track.images.coverart}}
-                        style={styles.albumCover}
-                      /> */}
-                      <View style={styles.trackInfo}>
-                        <Text style={styles.trackName}>
-                          {/* {item.track.title} */}
-                          tracktitle
-                        </Text>
-                        <Text style={styles.albumName}>
-                          {/* {item.track.subtitle} */}
-                          subtitle
-                        </Text>
-                      </View>
-                      <Entypo name="controller-play" size={24} color="white" />
+                    fontWeight: '500',
+                    width: '85%',
+                    color: 'white',
+                  }}
+                  onChangeText={setSearchText}
+                  onSubmitEditing={{handleSearch}}
+                  placeholder="Find in Liked songs"
+                />
+                <AntDesign name="search1" size={25} color="white" />
+              </TouchableOpacity>
+            </TouchableOpacity>
+          </View>
+          <View style={{height: 50}} />
+          <View style={{marginHorizontal: 10}}>
+            <Text style={{fontSize: 18, color: 'white', fontWeight: 'bold'}}>
+              {' '}
+              Liked Songs
+            </Text>
+            <Text style={{fontSize: 13, color: 'white', marginTop: 5}}>
+              {' '}
+              {searchedTracks.length} Songs
+            </Text>
+          </View>
+          <Pressable
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              marginHorizontal: 10,
+            }}>
+            <TouchableOpacity
+              style={{
+                height: 30,
+                width: 30,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#1DB954',
+                borderRadius: 18,
+              }}>
+              <AntDesign name="arrowdown" size={20} color="white" />
+            </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 10,
+              }}>
+              <TouchableOpacity>
+                <FontAwesome6
+                  name="arrow-down-up-across-line"
+                  size={25}
+                  color="#1DB954"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  height: 60,
+                  width: 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#1DB954',
+                  borderRadius: 30,
+                }}>
+                <Entypo name="controller-play" size={30} color="white" />
+              </TouchableOpacity>
+            </View>
+          </Pressable>
+          {loading ? (
+            <ActivityIndicator size={'large'} color={'gray'} />
+          ) : (
+            <FlatList
+              data={searchedTracks}
+              keyExtractor={item => item?.track?.key}
+              style={{marginTop: 10}}
+              renderItem={({item}) => (
+                <Pressable
+                // onPress={() => handlePlay(item)}
+                >
+                  <View style={styles.trackContainer}>
+                    <Image
+                      source={{uri: item?.track?.images?.coverart}}
+                      style={styles.albumCover}
+                    />
+                    <View style={styles.trackInfo}>
+                      <Text style={styles.trackName}>
+                        {item?.track?.title}
+                        tracktitle
+                      </Text>
+                      <Text style={styles.albumName}>
+                        {item?.track?.subtitle}
+                        subtitle
+                      </Text>
                     </View>
-                  </Pressable>
-                )}
-              />
-            )}
-          </ScrollView>
+                    <Entypo name="controller-play" size={24} color="white" />
+                  </View>
+                </Pressable>
+              )}
+            />
+          )}
         </SafeAreaView>
       </LinearGradient>
       {/* <TouchableOpacity
@@ -269,11 +267,9 @@ const LikedSongScreen = () => {
 
           <View style={{padding: 10, marginTop: 20}}>
             <Image
-              source={
-                {
-                  // uri: selectedTrack?.images.coverart,
-                }
-              }
+              source={{
+                uri: selectedTrack?.images.coverart,
+              }}
               style={{width: '100%', height: 330, borderRadius: 4}}
             />
             <View
